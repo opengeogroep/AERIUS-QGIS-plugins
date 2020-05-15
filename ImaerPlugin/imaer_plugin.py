b965fd73cfaa328c9146dc22b97b4a2d6522f708
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-05-15 18:40:37 +0200

    fix for improper plugin unload

--- ImaerPlugin/imaer_plugin.py
+++ ImaerPlugin/imaer_plugin.py
@@ -13,8 +13,10 @@
 
 import os
 
-from PyQt5.QtWidgets import QAction, QMessageBox
+from PyQt5.QtWidgets import QAction
 from PyQt5.QtGui import QIcon
+
+from qgis.core import QgsMessageLog, Qgis
 
 
 
@@ -23,23 +25,34 @@ class ImaerPlugin:
     def __init__(self, iface):
         self.iface = iface
         self.plugin_dir = os.path.dirname(__file__)
+        self.do_log = True
+        #self.log('init')
 
 
     def initGui(self):
         self.toolbar = self.iface.addToolBar("Imaer Toolbar")
 
         reader_icon = QIcon(os.path.join(self.plugin_dir, 'icon_reader.png'))
-        reader_action = QAction(reader_icon, 'Import IMAER result', self.iface.mainWindow())
-        reader_action.triggered.connect(self.run)
-        self.toolbar.addAction(reader_action)
+        self.reader_action = QAction(reader_icon, 'Import IMAER result', self.iface.mainWindow())
+        self.reader_action.triggered.connect(self.run)
+        self.toolbar.addAction(self.reader_action)
+
+        #self.log('initGui')
 
 
     def unload(self):
-        reader_action.triggered.disconnect(self.run)
-        self.toolbar.removeAction(reader_action)
+        self.reader_action.triggered.disconnect(self.run)
+        self.toolbar.removeAction(self.reader_action)
         del self.reader_action
         del self.toolbar
 
+        #self.log('unload')
+
 
     def run(self):
-        QMessageBox.information(None, 'Imaer plugin', 'Do something useful here')
+        self.log('Print this in the Imaer tab')
+
+
+    def log(self, message, tab='Imaer'):
+        if self.do_log:
+            QgsMessageLog.logMessage(str(message), tab, level=Qgis.Info)
