dd0245b6a009a330f888d280655fb93626f1037b
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-05-15 17:45:35 +0200

    plugin icon

--- ImaerPlugin/imaer_plugin.py
+++ ImaerPlugin/imaer_plugin.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
-
 ################################################################################
-# A QGIS plugin for generating AERIUS Calculator input IMAER files and
-# importing AERIUS Calculator output IMAER files.
 #
 # begin:      2020-05-08
 # copyright:  (C) 2020 by OpenGeoGroep
@@ -14,8 +11,26 @@
 # (at your option) any later version.
 ################################################################################
 
+from PyQt5.QtWidgets import QAction, QMessageBox
 
-from .imaer_plugin import ImaerPlugin
 
-def classFactory(iface):
-    return ImaerPlugin(iface)
+
+
+class ImaerPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+
+
+    def initGui(self):
+        self.action = QAction('Go!', self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+
+
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+        del self.action
+
+
+    def run(self):
+        QMessageBox.information(None, 'Imaer plugin', 'Do something useful here')
