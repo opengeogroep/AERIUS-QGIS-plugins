3cc32ce57182f714fc58c1a2217cf3c6925d70d0
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-05-15 18:14:42 +0200

    toolbar and reader action

--- ImaerPlugin/imaer_plugin.py
+++ ImaerPlugin/imaer_plugin.py
@@ -11,7 +11,10 @@
 # (at your option) any later version.
 ################################################################################
 
+import os
+
 from PyQt5.QtWidgets import QAction, QMessageBox
+from PyQt5.QtGui import QIcon
 
 
 
@@ -19,17 +22,23 @@ from PyQt5.QtWidgets import QAction, QMessageBox
 class ImaerPlugin:
     def __init__(self, iface):
         self.iface = iface
+        self.plugin_dir = os.path.dirname(__file__)
 
 
     def initGui(self):
-        self.action = QAction('Go!', self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
-        self.iface.addToolBarIcon(self.action)
+        self.toolbar = self.iface.addToolBar("Imaer Toolbar")
+
+        reader_icon = QIcon(os.path.join(self.plugin_dir, 'icon_reader.png'))
+        reader_action = QAction(reader_icon, 'Import IMAER result', self.iface.mainWindow())
+        reader_action.triggered.connect(self.run)
+        self.toolbar.addAction(reader_action)
 
 
     def unload(self):
-        self.iface.removeToolBarIcon(self.action)
-        del self.action
+        reader_action.triggered.disconnect(self.run)
+        self.toolbar.removeAction(reader_action)
+        del self.reader_action
+        del self.toolbar
 
 
     def run(self):
