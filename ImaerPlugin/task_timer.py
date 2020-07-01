35f41af079b860d65303228c8abcfea8c1e54bc1
Raymond Nijssen <r.nijssen@terglobo.nl>
2020-07-01 23:04:33 +0200

    tasktimer and import speedup

---
+++ ImaerPlugin/task_timer.py
@@ -0,0 +1,57 @@
+import time
+
+
+
+class TaskTimer():
+    def __init__(self):
+        self.reset()
+        self.add_feat_times = []
+
+
+    def show(self):
+        result = []
+        if self.start_time is not None:
+            total_time = time.time() - self.start_time
+            for task in self.tasks:
+                line = '{}: {} ({}%)'.format(task, self.tasks[task], (self.tasks[task] / total_time) * 100)
+                result.append(line)
+                print(line)
+        else:
+            print('nothing')
+        return result
+
+
+    def reset(self):
+        self.start_time = None
+        self.prev_time = None
+        self.prev_name = None
+        self.tasks = {}
+
+
+    def log(self, name):
+        #print(name)
+        #print(self.tasks)
+        if self.prev_time is not None:
+            time_delta = time.time() - self.prev_time
+            if self.prev_name in self.tasks:
+                self.tasks[self.prev_name] += time_delta
+            else:
+                self.tasks[self.prev_name] = time_delta
+            if self.prev_name == 'add feature':
+                self.add_feat_times.append(time_delta)
+        else:
+            self.start_time = self.prev_time = time.time()
+        self.prev_time = time.time()
+        self.prev_name = name
+
+if __name__ == '__main__':
+    tt = TaskTimer()
+
+    for i in range(10):
+        tt.log('a')
+        time.sleep(0.01)
+        tt.log('b')
+        time.sleep(0.02)
+    tt.log('b')
+
+    tt.show()
