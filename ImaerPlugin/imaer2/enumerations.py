c10ad631d5fd315af033c9be0150dacd12bc2412
Raymond Nijssen <r.nijssen@terglobo.nl>
2021-09-28 10:11:26 +0200

    removed old imaer versions (2 and 3)

--- ImaerPlugin/imaer2/enumerations.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class OutflowDirectionType(Enum):
-    VERTICAL = 1
-    HORIZONTAL = 2
--- ImaerPlugin/imaer3/__init__.py
+++
@@ -1,6 +0,0 @@
-#from .factory import ImaerFactory
-from .feature_collection_calculator import FeatureCollectionCalculator
-#from metadata import AeriusCalculatorMetadata
-#from imaer_document import ImaerDocument
-from generic import GuiNode
-#from .gml import GmlWriter
