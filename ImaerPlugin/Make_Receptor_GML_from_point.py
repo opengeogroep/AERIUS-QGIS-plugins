f93ce415b9b80d8d00c33e2829214552bf2ba3c9
Nicola Masey <nicola.masey@ricardo.com>
2022-09-30 12:09:14 +0100

    Added ui selction for receptor labels - still to add info to gml

--- ImaerPlugin/Make_Receptor_GML_from_point.py
+++ ImaerPlugin/Make_Receptor_GML_from_point.py
@@ -77,20 +77,14 @@ class GenerateReceptorGMLDialog(QDialog, FORM_CLASS):
         for fcb in self.findChildren(QgsFieldComboBox):
             fcb.setAllowEmptyFieldName(True)
 
-        #self.set_fixed_options()
         self.update_field_combos()
         self.update_ok_button()
-        #self.set_emission_tab()
-
+        
 
     def __del__(self):
         self.edit_outfile.textChanged.disconnect(self.update_ok_button)
-        self.combo_sector.currentIndexChanged.disconnect(self.set_emission_tab)
         self.combo_layer.layerChanged.disconnect(self.update_field_combos)
         self.button_outfile.clicked.disconnect(self.browse_generate_calc_input_file)
-
-        #self.btn_save_settings.clicked.disconnect(self.save_settings)
-        #self.btn_load_settings.clicked.disconnect(self.load_settings)
 
 
     def browse_generate_calc_input_file(self):
@@ -103,65 +97,17 @@ class GenerateReceptorGMLDialog(QDialog, FORM_CLASS):
         self.edit_outfile.setText(gml_outfn)
 
 
-    '''
-    def set_fixed_options(self):
-        # crs
-        for crs in ui_settings['crs']:
-            self.combo_crs.addItem(crs['name'], crs['srid'])
-
-        # sectors
-        self.combo_sector.addItem('<Select sector>', 0)
-        for sector_name in emission_sectors:
-            ##print(sector_name)
-            self.combo_sector.addItem(sector_name)
-
-        # project
-        for item in ui_settings['project_years']:
-            self.combo_project_year.addItem(item, item)
-        self.combo_project_year.setCurrentText(ui_settings['project_default_year'])
-
-        # situation
-        self.edit_situation_name.setText(ui_settings['situation_name'])
-        for item in ui_settings['situation_types']:
-            self.combo_situation_type.addItem(item, item)
-    '''
-
-    '''
-    def set_emission_tab(self):
-        # Remove all tabs but 'Metadata'
-        while self.tabWidget.count() > 1:
-            self.tabWidget.removeTab(1)
-        # Add selected emission tab
-        sector = self.combo_sector.currentText()
-        if sector in self.emission_tabs:
-            self.tabWidget.insertTab(1, self.emission_tabs[sector], sector)
-            self.tabWidget.setCurrentIndex(1)
-    '''
-
     def update_field_combos(self):
-        #for fcb in self.findChildren(QgsFieldComboBox):
-        #    fcb.setLayer(self.combo_layer.currentLayer())
-        layers = self.get_layer_list()
-        #enable_ok_button = len(layers) >= 2
-        #self.buttonBox.button(QDialogButtonBox.Ok).setEnabled(enable_ok_button)
-
+        for fcb in self.findChildren(QgsFieldComboBox):
+            fcb.setLayer(self.combo_layer.currentLayer())
+        
     def get_layer_list(self):
         '''Returns a list of selected deposition layers in the enabled comboBoxes'''
         result = []
-        '''
-        for key, widget in self.layer_widgets.items():
-            if widget.isEnabled():
-                layer = widget.currentLayer()
-                if layer is not None:
-                    if self.is_point_layer(layer):
-                        widget.setStyleSheet("QgsMapLayerComboBox { color : black; }")
-                        result.append(layer)
-                    else:
-                        widget.setStyleSheet("QgsMapLayerComboBox { color : red; }")
-        '''
-        layerList = QgsProject.instance().mapLayers().values()#QgsProject.instance().layerTreeRoot().findLayers()
+        
+        layerList = QgsProject.instance().mapLayers().values()
         for layer in layerList:
-            #if layer.geometryType() == QgsWkbTypes.PointGeometry:
+            
             if layer.wkbType() == 1:
                 result.append(layer)
 
@@ -169,7 +115,7 @@ class GenerateReceptorGMLDialog(QDialog, FORM_CLASS):
 
     def update_ok_button(self):
         pass
-        # getting error - 'NoneType' object has no attribute 'setEnabled' when have this code in place. Come back to  
+         
         '''
         if self.edit_outfile.text() == '':
             self.buttonBox.button(QDialogButtonBox.Save).setEnabled(False)
@@ -186,21 +132,6 @@ class GenerateReceptorGMLDialog(QDialog, FORM_CLASS):
         #name can be udpated later
         #
 
-        '''
-        # open a popup box and let the user select the shapefile to be converted
-        shp_fn, filter = self.nicolas_function_dialog.getOpenFileName(caption="Open a point shp file ", filter='*.shp', parent=self.iface.mainWindow())
-        self.log(f'run shp: {shp_fn}', user='dev')
-
-        # load the shapefile in QGIS
-        if os.path.exists(shp_fn):
-            name_of_file = os.path.basename(shp_fn)[:-4]
-            eq_layer=QgsVectorLayer(shp_fn,name_of_file,"ogr")
-            QgsProject.instance().addMapLayer(eq_layer)
-        else:
-            self.log("Unable to read the shp at the path provided")
-            return
-        '''
-        
         # get the generic gml file loaded
         imaer_doc = ImaerDocument()
         
@@ -263,51 +194,7 @@ class GenerateReceptorGMLDialog(QDialog, FORM_CLASS):
 
         return es
 
-        '''
-        from PyQt5.QtXml import QDomDocument        
         
-        doc = QDomDocument()
-        result = doc.createElement('imaer:CalculationPoint')
-        result.setAttribute('gml:id', str("CP.{}".format(str(local_id))))
-        doc.appendChild(result)
-
-        # identifier
-        ident_elem = doc.createElement('imaer:identifier')
-        nen_elem = doc.createElement('imaer:NEN3610ID')
-
-        elem = doc.createElement('imaer:namespace')
-        elem.appendChild(doc.createTextNode('NL.IMAER'))
-        nen_elem.appendChild(elem)
-
-        elem = doc.createElement('imaer:localId')
-        elem.appendChild(doc.createTextNode(str("CP."+ str(local_id))))
-
-        nen_elem.appendChild(elem)
-        ident_elem.appendChild(nen_elem)
-        result.appendChild(ident_elem)
-
-
-        # now move to the point data
-        # this isn't quite right as the gml:Point is the element but with a bit of a string
-        ident_elem = doc.createElement('imaer:GM_Point')
-        epsg_id = 28992 ##### HARD CODED JUST NOW
-        nen_elem = doc.createElement(str('gml:Point srsName="urn:ogc:def:crs:EPSG::'+str(epsg_id)+'" gml:id="CP.' + str(local_id) +'.POINT"'))
-        # the above should use the get_gml_elem function from gml.py to get the string I need I think (but relies on having actual geom)
-        # come back to this later when have an actual gml being written out
-        elem = doc.createElement('gml:pos')
-        elem.appendChild(doc.createTextNode(str(geom)))
-        nen_elem.appendChild(elem)
-
-        ident_elem.appendChild(nen_elem)
-        result.appendChild(ident_elem)
-
-        # add a label
-        ident_elem = doc.createElement('imaer:label')
-        ident_elem.appendChild(doc.createTextNode('Assessment point {}'.format(str(local_id))))
-        result.appendChild(ident_elem)
-
-        return result
-        '''
     def make_single_part(self, geom):
         '''Returns single part geometry or None if input has more than 1 part'''
         parts = geom.asGeometryCollection()
