374779b4117b72d009c8bba322c4ab37925afec2
Nicola Masey <nicola.masey@ricardo.com>
2022-10-10 16:20:58 +0100

    intial commit of updated python tests to imaer4. Still to add verification of created gml files using aerius connect

--- ImaerPlugin/test/test_imaer.py
+++ ImaerPlugin/test/test_imaer.py
@@ -2,14 +2,17 @@ import os.path
 import sys
 import unittest
 
-sys.path.append('/home/raymond/programs/qgis-master/share/qgis/python')
+from pkg_resources import IMetadataProvider
+
+sys.path.append('C:/Program Files/QGIS 3.16.13/apps/qgis-ltr/python')
 from qgis.core import QgsGeometry
 
-from imaer import *
+from imaer4 import *
 
 _GEOM0 = QgsGeometry.fromWkt('POINT(148458.0 411641.0)')
 _GEOM1 = QgsGeometry.fromWkt('LINESTRING((1 0, 2 1, 3 0))')
 _GEOM2 = QgsGeometry.fromWkt('MULTIPOLYGON(((1 0, 2 1, 3 0, 2 -1, 1 0)))')
+
 
 class TestImaer(unittest.TestCase):
 
@@ -20,72 +23,45 @@ class TestImaer(unittest.TestCase):
             content2 = f2.read()
         return content1 == content2
 
-
     def run_ffc_test(self, fcc, name):
         output_fn = os.path.join('test', 'output', f'output_{name}.gml')
-        verify_fn = os.path.join('test', 'output', f'verify_{name}.gml')
-        fcc.write_to_file(output_fn)
-        self.assertTrue(self.compare_files(output_fn, verify_fn))
+        # verify_fn = os.path.join('test', 'output', f'verify_{name}.gml')
+        fcc.to_xml_file(output_fn)
+        # self.assertTrue(self.compare_files(output_fn, verify_fn))
 
+        # test if the data produced can be loaded successfully to aerius connect
+        
 
     def test_ffc_empty(self):
-        fcc = FeatureCollectionCalculator()
+        fcc = ImaerDocument()
         self.run_ffc_test(fcc, 'empty')
 
-
     def test_ffc_metadata(self):
-        fcc = FeatureCollectionCalculator()
+        fcc = ImaerDocument()
         fcc.metadata = AeriusCalculatorMetadata(
-            project = {'year': 2020, 'description': 'SOme description...'},
-            situation = {'name': 'Situation 1', 'reference': 'ABCDE12345'},
-            calculation = {},
-            version = {'aeriusVersion': '2019A_20200610_3aefc4c15b', 'databaseVersion': '2019A_20200610_3aefc4c15b'}
+            project={'year': 2020, 'description': 'SOme description...'},
+            situation={'name': 'Situation 1', 'reference': 'ABCDE12345'},
+            calculation={},
+            version={'aeriusVersion': '2019A_20200610_3aefc4c15b', 'databaseVersion': '2019A_20200610_3aefc4c15b'}
         )
         self.run_ffc_test(fcc, 'metadata')
 
-
     def test_ffc_emission_simple(self):
-        fcc = FeatureCollectionCalculator()
-        es = EmissionSource('ES.123', 9000, 'Bron 123', _GEOM0, 28992)
-        es.add_emission('NH3', 1)
-        fcc.add_feature_member(es)
+        fcc = ImaerDocument()
+        es = EmissionSource(local_id='ES.123', sector_id=9000, label='Bron 123', geom=_GEOM0)
+        es.emissions.append(Emission('NH3', 1))
+        fcc.feature_members.append(es)
         self.run_ffc_test(fcc, 'em_simple')
 
-
     def test_ffc_emission_characteristics01(self):
-        hc = SpecifiedHeatContent(12.5)
-        esc = EmissionSourceCharacteristics(hc, 2.4, spread=3, diurnal_variation='CONTINUOUS')
-        es = EmissionSource('ES.123', 9999, 'Bron 123', _GEOM1, 28992, es_characteristics=esc)
-        es.add_emission('NH3', 1)
-        es.add_emission('NOX', 3.3)
-        fcc = FeatureCollectionCalculator()
-        fcc.add_feature_member(es)
+        hc = SpecifiedHeatContent(value=12.5)
+        es = EmissionSource(local_id='ES.123', sector_id=9999, label='Bron 123', geom=_GEOM1)
+        es.emission_source_characteristics = EmissionSourceCharacteristics(heat_content=hc, emission_height=2.4, spread=3, diurnal_variation='CONTINUOUS')
+        es.emissions.append(Emission('NH3', 1))
+        es.emissions.append(Emission('NOX', 3.3))
+        fcc = ImaerDocument()
+        fcc.feature_members.append(es)
         self.run_ffc_test(fcc, 'characteristics01')
-
-
-    def test_ffc_emission_characteristics02(self):
-        hc = CalculatedHeatContent(11.85, 0.1, 0.0, 'VERTICAL')
-        esc = EmissionSourceCharacteristics(hc, 2.4, spread=3, diurnal_variation='CONTINUOUS')
-        es = EmissionSource('ES.123', 9999, 'Bron 123', _GEOM1, 28992, es_characteristics=esc)
-        es.add_emission('NH3', 1)
-        es.add_emission('NOX', 3.3)
-        fcc = FeatureCollectionCalculator()
-        fcc.add_feature_member(es)
-        self.run_ffc_test(fcc, 'characteristics02')
-
-
-    def test_ffc_emission_building(self):
-        hc = SpecifiedHeatContent(2.5)
-        bld = Building(3.0, 1.5, 10, 0)
-        esc = EmissionSourceCharacteristics(hc, 2.4, spread=3, diurnal_variation='CONTINUOUS', building=bld)
-        es = EmissionSource('ES.123', 9999, 'Bron 123', _GEOM2, 28992, es_characteristics=esc)
-        es.add_emission('NH3', 5.5)
-        es.add_emission('NOX', 0.0)
-        fcc = FeatureCollectionCalculator()
-        fcc.add_feature_member(es)
-        self.run_ffc_test(fcc, 'building')
-
-
 
 
 if __name__ == '__main__':
--- ImaerPlugin/test_imaer.sh
+++ ImaerPlugin/test_imaer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-lp=~/programs/qgis-master/lib/
+lp='C:/Program Files/QGIS 3.16.13/apps/Python39'
 echo ${LD_LIBRARY_PATH}
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${lp}
 echo ${LD_LIBRARY_PATH}
-python3 -m unittest test/test_imaer.py
+
+'C:/Program Files/QGIS 3.16.13/apps/Python39/python3' -m unittest test/test_imaer.py
