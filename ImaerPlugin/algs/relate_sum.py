33f4774c0a27870f625dae8d250122808d997825
raymondnijssen <r.nijssen@terglobo.nl>
2023-01-26 12:43:49 +0100

    Processing algorithm for relate sum

--- ImaerPlugin/algs/relate_sum.py
+++ ImaerPlugin/algs/relate_sum.py
@@ -1,34 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""
-***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************
-"""
-
 from qgis.PyQt.QtCore import QCoreApplication
 from qgis.core import (QgsProcessing,
                        QgsFeatureSink,
                        QgsProcessingException,
                        QgsProcessingAlgorithm,
-                       QgsProcessingParameterFeatureSource,
+                       QgsProcessingParameterBoolean,
+                       QgsProcessingParameterMultipleLayers,
                        QgsProcessingParameterFeatureSink)
 from qgis import processing
 
 from .relate import RelateAlgorithm
 
-class RelateSumAlgorithm(RelateAlgorithm):
-    # Constants used to refer to parameters and outputs. They will be
-    # used when calling the algorithm from another algorithm, or when
-    # calling from the QGIS console.
 
-    INPUT1 = 'INPUT1'
-    INPUT2 = 'INPUT2'
+class RelateSumAlgorithm(RelateAlgorithm):
+    INPUT_LAYERS = 'INPUT_LAYERS'
+    ADD_TOTALS = 'ADD_TOTALS'
     OUTPUT = 'OUTPUT'
 
     def tr(self, string):
@@ -38,143 +25,97 @@ class RelateSumAlgorithm(RelateAlgorithm):
         return RelateSumAlgorithm()
 
     def name(self):
-        """
-        Returns the algorithm name, used for identifying the algorithm. This
-        string should be fixed for the algorithm, and must not be localised.
-        The name should be unique within each provider. Names should contain
-        lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
         return 'relate_sum'
 
     def displayName(self):
-        """
-        Returns the translated algorithm name, which should be used for any
-        user-visible display of the algorithm name.
-        """
         return self.tr('Sum')
 
     def group(self):
-        """
-        Returns the name of the group this algorithm belongs to. This string
-        should be localised.
-        """
         return self.tr('Relate depositions')
 
     def groupId(self):
-        """
-        Returns the unique ID of the group this algorithm belongs to. This
-        string should be fixed for the algorithm, and must not be localised.
-        The group id should be unique within each provider. Group id should
-        contain lowercase alphanumeric characters only and no spaces or other
-        formatting characters.
-        """
         return 'relate'
 
     def shortHelpString(self):
-        """
-        Returns a localised short helper string for the algorithm. This string
-        should provide a basic description about what the algorithm does and the
-        parameters and outputs associated with it..
-        """
-        return self.tr("Calculate sum of deposition layers")
+        return self.tr("Summarize values of deposition layers")
 
     def initAlgorithm(self, config=None):
-        """
-        Here we define the inputs and output of the algorithm, along
-        with some other properties.
-        """
-
-        # We add the input vector features source. It can have any kind of
-        # geometry.
         self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.INPUT1,
-                self.tr('Input layer 1'),
-                [QgsProcessing.TypeVectorAnyGeometry]
+            QgsProcessingParameterMultipleLayers(
+                self.INPUT_LAYERS,
+                self.tr('Input deposition layers'),
+                QgsProcessing.TypeVectorPolygon,
+                optional=False
             )
         )
         self.addParameter(
-            QgsProcessingParameterFeatureSource(
-                self.INPUT2,
-                self.tr('Input layer 2'),
-                [QgsProcessing.TypeVectorAnyGeometry]
+            QgsProcessingParameterBoolean(
+                self.ADD_TOTALS,
+                self.tr('Add field with total depositions'),
+                defaultValue=True
             )
         )
-
-        # We add a feature sink in which to store our processed features (this
-        # usually takes the form of a newly created vector layer when the
-        # algorithm is run in QGIS).
         self.addParameter(
             QgsProcessingParameterFeatureSink(
                 self.OUTPUT,
-                self.tr('Output layer')
+                self.tr('Deposition sum')
             )
         )
 
     def processAlgorithm(self, parameters, context, feedback):
-        # Retrieve the feature source and sink. The 'dest_id' variable is used
-        # to uniquely identify the feature sink, and must be included in the
-        # dictionary returned by the processAlgorithm function.
-        layer_1 = self.parameterAsSource(parameters, self.INPUT1, context)
-        if layer_1 is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
-        if not self._is_dep_layer(layer_1):
-            feedback.pushInfo('Layer 1 is not a dep layer')
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+        feedback.setProgress(0)
 
-        layer_2 = self.parameterAsSource(parameters, self.INPUT2, context)
-        if layer_2 is None:
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
-        if not self._is_dep_layer(layer_2):
-            feedback.pushInfo('Layer 1 is not a dep layer')
-            raise QgsProcessingException(self.invalidSourceError(parameters, self.INPUT))
+        layers = self.parameterAsLayerList(parameters, self.INPUT_LAYERS, context)
+        step = 50 / len(layers)
+        current = 1
 
-        output_fields = self._get_output_fields()
+        self.geometry_cache = {}
+
+        dep_dict_layers = []
+        for layer in layers:
+            layer_name = layer.name()
+            if not self._is_dep_layer(layer):
+                raise QgsProcessingException(f'"{layer_name}" is not a valid deposition layer.')
+            dep_dict_layer = self._create_receptor_dictionary(layer)
+            if dep_dict_layer is None:
+                raise QgsProcessingException(f'"{layer_name}" is not a valid deposition layer.')
+            dep_dict_layers.append(dep_dict_layer)
+
+            feedback.setProgress(int(current * step))
+            current +=1
+
+        add_totals = self.parameterAsBoolean(parameters, self.ADD_TOTALS, context)
+        output_fields = self._get_output_fields(with_totals=add_totals)
+
         (sink, dest_id) = self.parameterAsSink(
             parameters,
             self.OUTPUT,
             context,
             output_fields,
-            layer_1.wkbType(),
-            layer_1.sourceCrs()
+            layers[0].wkbType(),
+            layers[0].sourceCrs()
         )
 
         if sink is None:
             raise QgsProcessingException(self.invalidSinkError(parameters, self.OUTPUT))
 
-        self.geometry_cache = {}
-
-        dep_dict_layer_1 = self._create_receptor_dictionary(layer_1)
-        if dep_dict_layer_1 is None:
-            layer_name = layer_1.name()
-            feedback.pushError(f'"{layer_name}" is not a valid deposition layer.')
-
-        dep_dict_layer_2 = self._create_receptor_dictionary(layer_2)
-        if dep_dict_layer_2 is None:
-            layer_name = layer_2.name()
-            feedback.pushError(f'"{layer_name}" is not a valid deposition layer.')
-
         #qml_file_name = os.path.join(self.plugin.plugin_dir, 'styles', 'calc_result_diff.qml')
-        calc_result_dict = self._calc_dict_sum([dep_dict_layer_1, dep_dict_layer_2])
+        calc_result_dict = self._calc_dict_sum(dep_dict_layers)
 
         if len(calc_result_dict) == 0:
-            feedback.pushInfo(f'No result features to load.')
-            return #  TODO Find good way to terminate process.
+            raise QgsProcessingException(f'No result features to load.')
 
-        total = 100.0 / len(calc_result_dict)
+        step = 50 / len(calc_result_dict)
         current = 1
 
         for receptor_id in calc_result_dict:
-            # Stop the algorithm if cancel button has been clicked
             if feedback.isCanceled():
                 break
 
-            feat = self._create_result_feature(receptor_id, calc_result_dict[receptor_id], 6)
+            feat = self._create_result_feature(receptor_id, calc_result_dict[receptor_id], max_decimals=6, with_totals=add_totals)
             sink.addFeature(feat, QgsFeatureSink.FastInsert)
 
-            # Update the progress bar
-            feedback.setProgress(int(current * total))
+            feedback.setProgress(50 + int(current * step))
             current += 1
 
         return {self.OUTPUT: dest_id}
