f564b481b2c94850a70aca00271e3612d5d7bac1
Nicola Masey <nicola.masey@ricardo.com>
2023-05-16 15:37:36 +0100

    moved StandardVehicle and Vehicle from roads_srm2 and roads_adms into roads.py

--- ImaerPlugin/imaer5/roads.py
+++ ImaerPlugin/imaer5/roads.py
@@ -26,3 +26,54 @@ class RoadEmissionSource(EmissionSourceType):
             result.appendChild(elem)
 
         return result
+
+class Vehicle(object):
+
+    def __init__(self, *, vehicles_per_time_unit=None, time_unit=None, **kwargs):
+        self.vehicles_per_time_unit = vehicles_per_time_unit
+        self.time_unit = time_unit
+
+    def to_xml_elem(self, doc=QDomDocument()):
+
+        class_name = self.__class__.__name__
+        result = doc.createElement(f'imaer:{class_name}')
+
+        elem = doc.createElement('imaer:vehiclesPerTimeUnit')
+        elem.appendChild(doc.createTextNode(str(self.vehicles_per_time_unit)))
+        result.appendChild(elem)
+
+        elem = doc.createElement('imaer:timeUnit')
+        elem.appendChild(doc.createTextNode(str(self.time_unit)))
+        result.appendChild(elem)
+
+        return result
+
+
+class StandardVehicle(Vehicle):
+
+    def __init__(self, *, stagnation_factor=None, vehicle_type=None, maximum_speed=None, strict_enforcement=None, **kwargs):
+        super().__init__(**kwargs)
+        self.stagnation_factor = stagnation_factor
+        self.vehicle_type = vehicle_type
+        self.maximum_speed = maximum_speed
+        self.strict_enforcement = strict_enforcement
+
+    def to_xml_elem(self, doc=QDomDocument()):
+        result = super().to_xml_elem(doc)
+        result.setAttribute('vehicleType', self.vehicle_type)
+
+        elem = doc.createElement('imaer:stagnationFactor')
+        elem.appendChild(doc.createTextNode(str(self.stagnation_factor)))
+        result.appendChild(elem)
+
+        if self.maximum_speed is not None:
+            elem = doc.createElement('imaer:maximumSpeed')
+            elem.appendChild(doc.createTextNode(str(self.maximum_speed)))
+            result.appendChild(elem)
+
+        if self.strict_enforcement is not None:
+            elem = doc.createElement('imaer:strictEnforcement')
+            elem.appendChild(doc.createTextNode(str(self.strict_enforcement)))
+            result.appendChild(elem)
+
+        return result
